7c8cba45ce91ef5e8e85618fe59057606e23d319
Alexander.C <whitesnowoffice.dev@gmail.com>
2025-01-11 11:50:35 -0800

    test: button in elementsPage

--- src/pages/elements/index.js
+++ src/pages/elements/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 import Button from "../../components/button";
+import "./styles.scss";
 
 const ElementsPage = () => {
   return (
-    <>
+    <div className="elementsPage">
       <h3>Welcome to ElementsPage</h3>
-      <Button />
-    </>
+      <Button placeholder={"Darkpurple"} color={"darkpurple"} withIcon={true} />
+      <Button placeholder={"White"} color={"white"} />
+    </div>
   );
 };
 
