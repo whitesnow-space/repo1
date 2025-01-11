c518dc2528d2f47739630fe496f55f7f3777bac1
Alexander.C <whitesnowoffice.dev@gmail.com>
2025-01-11 11:49:41 -0800

    feature: add button component

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,7 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
+import cx from "classnames";
 
-const Button = () => {
-  return <button className="btn btn-primary">Click me!</button>;
+import "./styles.scss";
+
+const Button = ({ color, placeholder, withIcon = false }) => {
+  return (
+    <>
+      <button className={cx("button", `button-${color}`)} type="button">
+        <div className="d-flex">
+          <span className="button-placeholder">{placeholder}</span>
+          {withIcon && <i className="button-icon bi bi-arrow-right"></i>}
+        </div>
+      </button>
+    </>
+  );
+};
+
+Button.propTypes = {
+  placeholder: PropTypes.string,
+  color: PropTypes.oneOf(["darkpurple", "white"]),
+  withIcon: PropTypes.bool,
 };
 
 export default Button;
