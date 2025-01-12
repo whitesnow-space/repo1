18a1da77f596ee76a648c2224af89f8c1034639d
Alexander.C <whitesnowoffice.dev@gmail.com>
2025-01-12 05:33:35 -0800

    feature: add copyClipboard component

---
+++ src/components/copyClipboard/index.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+
+const CopyClipboard = ({ placeholder, type }) => {
+  const [copied, setCopied] = useState(false);
+
+  useEffect(() => {
+    if (copied) {
+      const timer = setTimeout(() => {
+        setCopied(false);
+      }, 1000); // Set timeout for 0.5 seconds
+
+      // Cleanup the timer when the component unmounts or `copied` changes
+      return () => clearTimeout(timer);
+    }
+  }, [copied]);
+
+  const notification = {
+    general: "Copied!",
+    address: "Email Address Copied!",
+  };
+
+  const copyText = async () => {
+    try {
+      await window.navigator.clipboard.writeText(placeholder);
+      setCopied(true);
+    } catch (err) {
+      console.error("Unable to copy to clipboard.", err);
+      alert("Copy to clipboard failed.");
+    }
+  };
+
+  return (
+    <span className="copyClipboard" onClick={() => copyText()}>
+      {!copied ? placeholder : notification[type]}
+    </span>
+  );
+};
+
+CopyClipboard.propTypes = {
+  placeholder: PropTypes.string,
+  type: PropTypes.oneOf(["general", "address"]),
+};
+
+export default CopyClipboard;
