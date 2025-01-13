4be2f022cf7812be1e66e62a2f4e4d4abb21a333
Alexander.C <whitesnowoffice.dev@gmail.com>
2025-01-12 18:19:16 -0800

    fix: update workPanelItem component style

--- src/components/custom/workPanel/workPanelItem/index.js
+++ src/components/custom/workPanel/workPanelItem/index.js
@@ -5,13 +5,13 @@ import "./styles.scss";
 const WorkPanelItem = ({ data }) => {
   return (
     <div className="workPanelItem row">
-      <div className="row workPanelItem-panel col-sm-6 col-md-12">
+      <div className="row workPanelItem-panel col-sm-7 col-md-12">
         <div className="d-flex justify-content-center align-items-center workPanelItem-number-panel col-3">
           <div className="workPanelItem-number">{data.number}</div>
         </div>
         <div className="workPanelItem-text darkpurple col-9">{data.text}</div>
       </div>
-      <div className="col-sm-6 col-md-12">
+      <div className="col-sm-5 col-md-12">
         <img src={data.imgUrl} alt="workPanel-img" />
       </div>
     </div>
