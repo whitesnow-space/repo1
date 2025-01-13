9502a2181c24f78418c98a3ac6284bed2db5bf36
Alexander.C <whitesnowoffice.dev@gmail.com>
2025-01-13 05:35:04 -0800

    feature: add simpleAccordion component

---
+++ src/components/elements/simpleAccordion/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Accordion } from "react-bootstrap";
+
+const SimpleAccordion = ({ data }) => {
+  return (
+    <div className="simpleAccordion">
+      <Accordion>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>
+            <span className="darkpurple fw-bold">{data.title}</span>
+          </Accordion.Header>
+          <Accordion.Body>
+            <span className="darkpurple">{data.description}</span>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </div>
+  );
+};
+
+SimpleAccordion.propTypes = {
+  data: PropTypes.object,
+};
+
+export default SimpleAccordion;
