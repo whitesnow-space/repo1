ceb5c978f22952feefa7ffb5b0041463e396c5bd
Alexander.C <whitesnowoffice.dev@gmail.com>
2025-01-12 17:14:03 -0800

    test: elements page with static data const

--- src/pages/elements/index.js
+++ src/pages/elements/index.js
@@ -6,21 +6,10 @@ import TextPanel from "../../components/elements/textPanel";
 import CopyClipboard from "../../components/elements/copyClipboard";
 import CustomImage from "../../components/elements/customImage";
 import CustomNavbar from "../../components/custom/navbar";
+import staticData from "../../consts/staticData"; /* staticData */
 import "./styles.scss";
 
 const ElementsPage = () => {
-  /* Static Data */
-  const staticData = {
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim adminim veniam, quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt inculpa qui officia deserunt mollit anim id est laborum.",
-    list: [
-      { title: "title-1", price: "KP-1" },
-      { title: "title-2", price: "KP-2" },
-      { title: "title-3", price: "KP-3" },
-    ],
-    imgUrl: "assets/images/img1.jpg",
-  };
-
   return (
     <div className="elementsPage">
       <h3>Welcome to ElementsPage</h3>
@@ -33,12 +22,7 @@ const ElementsPage = () => {
 
       <p>Accordion</p>
 
-      <CustomAccordion
-        type="care"
-        title="Car Wash"
-        price={55}
-        description={staticData.description}
-      />
+      <CustomAccordion type="care" title="Car Wash" price={55} />
       <CustomAccordion
         type="star"
         title="Additional Services"
