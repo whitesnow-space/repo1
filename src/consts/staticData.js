4574bf1efb7360d568a249aabbd66b50c66c11f7
Alexander.C <whitesnowoffice.dev@gmail.com>
2025-01-12 17:13:03 -0800

    feature: add static data

---
+++ src/consts/staticData.js
@@ -0,0 +1,111 @@
+const staticData = {
+  tireData: [
+    {
+      title: "Tire Changes",
+      description: "This is a service",
+      type: "tire",
+      price: 299,
+      isAdditionalService: false,
+    },
+    {
+      title: "Tire Hotel",
+      description: "This is a service",
+      type: "tire",
+      price: 299,
+      isAdditionalService: false,
+    },
+  ],
+  tireAdditionalData: [
+    {
+      title: "Extra car wash",
+      description: "This is a service",
+      type: "tire",
+      price: 299,
+      isAdditionalService: true,
+    },
+    {
+      title: "Extra car wash",
+      description: "This is a service",
+      type: "tire",
+      price: 299,
+      isAdditionalService: true,
+    },
+  ],
+  careData: [
+    {
+      title: "Extra car wash",
+      description: "This is a service",
+      type: "care",
+      price: 299,
+      isAdditionalService: false,
+    },
+    {
+      title: "Extra car wash",
+      description: "This is a service",
+      type: "care",
+      price: 299,
+      isAdditionalService: false,
+    },
+    {
+      title: "Extra car wash",
+      description: "This is a service",
+      type: "care",
+      price: 299,
+      isAdditionalService: false,
+    },
+  ],
+  careAdditionalData: [
+    {
+      title: "Extra car wash",
+      description: "This is a service",
+      type: "care",
+      price: 299,
+      isAdditionalService: true,
+    },
+    {
+      title: "Extra car wash",
+      description: "This is a service",
+      type: "care",
+      price: 299,
+      isAdditionalService: true,
+    },
+    {
+      title: "Extra car wash",
+      description: "This is a service",
+      type: "care",
+      price: 299,
+      isAdditionalService: true,
+    },
+    {
+      title: "Extra car wash",
+      description: "This is a service",
+      type: "care",
+      price: 299,
+      isAdditionalService: true,
+    },
+    {
+      title: "Extra car wash",
+      description: "This is a service",
+      type: "care",
+      price: 299,
+      isAdditionalService: true,
+    },
+    {
+      title: "Extra car wash",
+      description: "This is a service",
+      type: "care",
+      price: 299,
+      isAdditionalService: true,
+    },
+  ],
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim adminim veniam, quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt inculpa qui officia deserunt mollit anim id est laborum.",
+  list: [
+    { title: "title-1", price: "KP-1" },
+    { title: "title-2", price: "KP-2" },
+    { title: "title-3", price: "KP-3" },
+  ],
+  imgUrl: "assets/images/img1.jpg",
+};
+
+export default staticData;
