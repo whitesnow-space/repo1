759498bd06365593f57e0691ecdb3331574e70f1
Alexander.C <whitesnowoffice.dev@gmail.com>
2025-01-15 19:48:30 -0800

    fix: update button style

--- src/components/elements/button/index.js
+++ src/components/elements/button/index.js
@@ -8,7 +8,7 @@ const Button = ({ color, placeholder, withIcon = false }) => {
   return (
     <>
       <button className={cx("button", `button-${color}`)} type="button">
-        <div className="d-flex">
+        <div className="d-flex mt-1">
           <span className="button-placeholder">{placeholder}</span>
           {withIcon && <i className="button-icon bi bi-arrow-right"></i>}
         </div>
