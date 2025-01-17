300460e4f460964fb035ac7778c46602cae1f72c
Alexander.C <whitesnowoffice.dev@gmail.com>
2025-01-17 06:35:55 -0800

    feature: add path consts

--- src/consts/index.js
+++ src/consts/index.js
@@ -1,3 +1,4 @@
 export { default as ROUTES } from "./routes";
 export { default as ICONS } from "./icons.js";
 export { default as IMGURLS } from "./imgUrls.js";
+export { default as PATH } from "./path.js";
---
+++ src/consts/path.js
@@ -0,0 +1,11 @@
+const PATH = {
+  INDEX: "/",
+  PRICE: "/price",
+  SERVICE: "/service",
+  HELP: "/help",
+  BUSINESS: "/business",
+  BOOKING: "/booking",
+  ELEMENTS: "/elements",
+};
+
+export default PATH;
